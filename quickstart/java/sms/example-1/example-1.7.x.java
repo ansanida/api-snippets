bed39207230f2e5c6fdfb01fdf9a70b1985ca27b
ansanida <33883725+ansanida@users.noreply.github.com>
2019-03-09 22:44:48 -0500

    Update example-1.7.x.java

--- quickstart/java/sms/example-1/example-1.7.x.java
+++ quickstart/java/sms/example-1/example-1.7.x.java
@@ -6,16 +6,16 @@ import com.twilio.type.PhoneNumber;
 public class SmsSender {
     // Find your Account Sid and Auth Token at twilio.com/console
     public static final String ACCOUNT_SID =
-            "ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
+            "AC8c0acc2e85428d14e27d1d54b849e0f2";
     public static final String AUTH_TOKEN =
-            "your_auth_token";
+            "7fb7f636beca9a939fb3c1f6e5bd537b";
 
     public static void main(String[] args) {
         Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
 
         Message message = Message
-                .creator(new PhoneNumber("+14159352345"), // to
-                        new PhoneNumber("+14158141829"), // from
+                .creator(new PhoneNumber("+15183340698"), // to
+                        new PhoneNumber("+17325323669"), // from
                         "Where's Wallace?")
                 .create();
 
